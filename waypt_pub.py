79405647b96d62b0ceb1a4ca75b4acb3ad845401
Yash Mhaskar <mhaskaryash901@gmail.com>
2023-11-12 21:26:08 -0500

    Add files via upload

--- waypt_pub.py
+++ waypt_pub.py
@@ -6,9 +6,7 @@ from rclpy.node import Node
 from geometry_msgs.msg import PoseStamped
 from nav2_msgs.action._navigate_to_pose import NavigateToPose_FeedbackMessage
 from rclpy.qos import QoSProfile, QoSDurabilityPolicy, QoSReliabilityPolicy, QoSHistoryPolicy
-import numpy as np
 import math
-import time
 
 # Global Variables
 global state
@@ -35,66 +33,92 @@ class WayptPub(Node):
 		self.waypt_pub = self.create_publisher(PoseStamped, 'goal_pose', 10)
 		
 	def feedback_callback(self,msg):
-		feedback = msg.status.status
-		self.get_logger().info('feedback: "%s"'% feedback)
-		STATUS_SUCCEEDED = "Waypoint Reached" #change status_succeeded to whatever output says the waypoint is reached
+		global state
+		global reached
+		feedback = msg.feedback
+		#self.get_logger().info('feedback: "%s"'% feedback)
+		distance_remaining = feedback.distance_remaining
+		x_cur = feedback.current_pose.pose.position.x
+		y_cur = feedback.current_pose.pose.position.y
+		w = feedback.current_pose.pose.orientation.w
+
+		self.get_logger().info('Remaining distance: "%s"'% distance_remaining)
+		 # structured like poseStamped so calculate own error and use to determine states
+		
+		err_ran = 0.02 # error range
 
 		goal = PoseStamped()
+		goal.header.stamp = self.get_clock().now().to_msg()    #rclpy.Time.now()
+		goal.header.frame_id = "map"
+
+		goal.pose.position.z = 0.0
+		goal.pose.orientation.x = 0.0
+		goal.pose.orientation.y = 0.0
+		goal.pose.orientation.z = 0.0
+		goal.pose.orientation.w = 1.0
+		
 		if state == 1:
-			if feedback == STATUS_SUCCEEDED:
+			x_des = 3.9
+			y_des = -0.85
+			err =  math.sqrt((x_des-x_cur)**2 + (y_des-y_cur)**2)
+			self.get_logger().info('error: "%s"'% err)
+			if err<err_ran: # pick a new value based off burger.yaml file/tuning
 				state = 2
 			else:
 				self.get_logger().info('state: "%s"'% state)
-				goal.header.stamp = self.get_clock().now().to_msg()    #rclpy.Time.now()
-				goal.header.frame_id = "map"
-				goal.pose.position.x = 3.9
-				goal.pose.position.y = -0.85
-				goal.pose.position.z = 0.0
-
-				goal.pose.orientation.x = 0.0
-				goal.pose.orientation.y = 0.0
-				goal.pose.orientation.z = 0.0
-				goal.pose.orientation.w = 1.0
-				self.waypt_pub.publish(goal)
+				goal.pose.position.x = x_des
+				goal.pose.position.y = y_des
+				
 		elif state == 2:
-			if feedback == STATUS_SUCCEEDED:
+			x_des = 0.0
+			y_des = 0.0
+			err = math.sqrt((x_des-x_cur)**2 + (y_des-y_cur)**2)
+			self.get_logger().info('error: "%s"'% err)
+			if err<err_ran: # pick a new value based off burger.yaml file/tuning
 				state = 3
 			else:
 				self.get_logger().info('state: "%s"'% state)
-				goal.header.stamp = self.get_clock().now().to_msg()    #rclpy.Time.now()
-				goal.header.frame_id = "map"
-				goal.pose.position.x = 0.0
-				goal.pose.position.y = 0.0
-				goal.pose.position.z = 0.0
+				goal.pose.position.x = x_des
+				goal.pose.position.y = y_des
 
-				goal.pose.orientation.x = 0.0
-				goal.pose.orientation.y = 0.0
-				goal.pose.orientation.z = 0.0
-				goal.pose.orientation.w = 1.0
-				self.waypt_pub.publish(goal)
 		elif state == 3:
-			if feedback == STATUS_SUCCEEDED:
+			x_des = 3.9
+			y_des = -0.85
+			err = math.sqrt((x_des-x_cur)**2 + (y_des-y_cur)**2)
+			self.get_logger().info('error: "%s"'% err)
+			if err<err_ran: # pick a new value based off burger.yaml file/tuning
 				state = 4
 			else:
 				self.get_logger().info('state: "%s"'% state)
 				goal.header.stamp = self.get_clock().now().to_msg()    #rclpy.Time.now()
 				goal.header.frame_id = "map"
-				goal.pose.position.x = 3.9
-				goal.pose.position.y = -0.85
-				goal.pose.position.z = 0.0
-
-				goal.pose.orientation.x = 0.0
-				goal.pose.orientation.y = 0.0
-				goal.pose.orientation.z = 0.0
-				goal.pose.orientation.w = 1.0
-				self.waypt_pub.publish(goal)
+				goal.pose.position.x = x_des
+				goal.pose.position.y = y_des
 		else:
 				self.get_logger().info('All waypoints reached')
+		self.waypt_pub.publish(goal)
 	
 
 def main(args=None):
 	# Setting up publisher values
 	rclpy.init(args=args)
-	feedback_sub=WayptPub()
-	rclpy.spin(feedback_sub)
+	waypt_pub=WayptPub()
+	print('Running')
+	
+
+	goal = PoseStamped()
+	goal.header.frame_id = "map"
+	goal.pose.position.x = 3.9
+	goal.pose.position.y = -0.085
+	goal.pose.position.z = 0.0
+	goal.pose.orientation.x = 0.0
+	goal.pose.orientation.y = 0.0
+	goal.pose.orientation.z = 0.0
+	goal.pose.orientation.w = 1.0
+
+	for x in range(100):
+		waypt_pub.waypt_pub.publish(goal)
+		print('published')
+
+	rclpy.spin(waypt_pub)
 	rclpy.shutdown()
