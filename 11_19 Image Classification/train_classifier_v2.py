a27d39d4059e47df62c5424e132cdeeeeb062b17
Yash Mhaskar <mhaskaryash901@gmail.com>
2023-11-20 22:50:55 -0500

    Update train_classifier_v2.py

--- 11_19 Image Classification/train_classifier_v2.py
+++ 11_19 Image Classification/train_classifier_v2.py
@@ -50,9 +50,9 @@ def contour_extraction(path):
     high_V = 236
     min_size = 0.1
     # to actually visualize the effect of `CHAIN_APPROX_SIMPLE`, we need a proper image
-    image_filepath = '/home/michaelangelo/Documents/Team19_image_classifier-master/2023Simgs/S2023_imgs/015.png'
+ 
     #data, label = load_data(images_filepath, labels_filepath)
-    image1 = cv2.imread(image_filepath)
+    image1 = cv2.imread(path)
     #height, width, channels = image1.shape
     #x1,y1,x2,y2 = width/4, height/4, width-(width/4), height-(height/4)
     #image1 = image1[int(y1):int(y2), int(x1):int(x2)]
@@ -82,9 +82,6 @@ def contour_extraction(path):
     image_copy2 = image1.copy()
     cv2.drawContours(image_copy2, contours2, -1, (100, 200, 0), 2, cv2.LINE_AA)
     cv2.imshow('SIMPLE Approximation contours', image_copy2)
-    cv2.waitKey(0)
-    cv2.imshow('CHAIN_APPROX_SIMPLE Point only', frame_threshold)
-    cv2.destroyAllWindows()
     return contours2
 
 
@@ -94,7 +91,7 @@ def train_classifier():
 
     #filepath = rclpy.get_param('~file_name', 'S2023Labels.csv')
     images_filepath = '/home/michaelangelo/Documents/Team19_image_classifier-master/2023Simgs/S2023_imgs/'
-    labels_filepath = '/home/michaelangelo/Documents/Team19_image_classifier-master/2023Simgs/S2023Labels.csv'
+    labels_filepath = '/home/michaelangelo/Documents/Team19_image_classifier-master/2023Simgs/S2023_imgs/S2023Labels.csv'
     #if len(filepath) > 0 and filepath[0] != '/':
     #    rospack = rospkg.RosPack()
     #    filepath = rospack.get_path('image_classifier') + '/data/training/' + filepath
@@ -112,9 +109,36 @@ def train_classifier():
     #data_train, data_test, label_train, label_test = train_test_split(data, label, test_size=split) # randomizes the data into training and testing
     imagepath_train, imagepath_test, label_train, label_test = train_test_split(images_paths, label, test_size=split) # randomizes the data into training and testing
     
-    for path in imagepath_train:
+    contour_features_train = np.zeros((len(imagepath_train),3),dtype=float)
+    contour_features_test = np.zeros((len(imagepath_test),3),dtype=float)
+
+    for i,path in enumerate(imagepath_train):
         contours = contour_extraction(path)
-        
+        contours = sorted(contours, key=cv2.contourArea, reverse=True)
+        if len(contours)>0:
+            contour = contours[0]
+            contour_features_train[i] = np.array(calculate_features(contour),dtype=float)
+        else:
+            contour_features_train[i] =  np.array([0,0,0],dtype=float)
+    contour_features_train =  np.array(contour_features_train,dtype=float)
+
+
+    #features_train = [contour_features_train(vector).reshape(-1,3) for vector in contour_features_train]
+    features_train = contour_features_train
+
+    for i,path in enumerate(imagepath_test):
+        contours = contour_extraction(path)
+        contours = sorted(contours, key=cv2.contourArea, reverse=True)
+        contour = contours[0] if contours else None
+        if len(contours)>0:
+            contour = contours[0]
+            contour_features_test[i] = np.array(calculate_features(contour),dtype=float)
+        else:
+            contour_features_test[i] = np.array([0,0,0],dtype=float)
+
+    #features_test = [contour_features_test(vector).reshape(-1,3) for vector in contour_features_test]
+    features_test = contour_features_test
+    print(features_train)
 
     print( 'Training classifier...')
 
@@ -132,15 +156,14 @@ def train_classifier():
 
 
     # Feature extraction using color histogram
-    features_train = [extract_color_histogram(image).reshape(-1) for image in data_train]
-    print("Shape of a feature vector:", features_train[0].shape)
-    features_test = [extract_color_histogram(image).reshape(-1) for image in data_test]
+    #features_train = [extract_color_histogram(image).reshape(-1) for image in data_train]
+    #print("Shape of a feature vector:", features_train[0].shape)
+    #features_test = [extract_color_histogram(image).reshape(-1) for image in data_test]
 
     # Convert label strings to numerical values
     label_encoder = LabelEncoder()
     label_train_encoded = label_encoder.fit_transform(label_train)
     label_test_encoded = label_encoder.transform(label_test)
-
     # Initialize the classifier (RandomForestClassifier in this case)
     classifier = RandomForestClassifier(n_estimators=100, random_state=42)
     # End image processing code (You write this!)
@@ -148,7 +171,7 @@ def train_classifier():
 
     #print(data_train)
     classifier.fit(features_train, label_train_encoded)
-
+    print('nope')
     print('Detailed results on a %0.0f/%0.0f train/test split:' % ((1 - split)*100, split*100))
     predicted = classifier.predict(features_test)
     print(metrics.classification_report(label_test_encoded, predicted))
@@ -196,7 +219,6 @@ def load_data(images_filepath, labels_filepath):
     """Parse training data and labels from a .csv file."""
     labels_data = np.loadtxt(labels_filepath, delimiter=',', dtype=str)
     images_paths = [images_filepath + f'{number}.png' for number in labels_data[:,0]] #np.loadtxt(filepath, delimiter=',')
-    print(images_paths)
     #x = data[:, :data.shape[1] - 1]
     #y = data[:, data.shape[1] - 1]
     images=[cv2.imread(image_path) for image_path in images_paths]
@@ -207,5 +229,5 @@ def load_data(images_filepath, labels_filepath):
 if __name__ == '__main__':
     try:
         train_classifier()
-    except rclpy.ROSInterruptException:
+    except rclpy.exceptions.ROSInterruptException:
         pass
