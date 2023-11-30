6e9f110a467606f0a29ac5a40ff39db10f96571a
jsommer33 <146038709+jsommer33@users.noreply.github.com>
2023-11-30 12:48:50 -0500

    Update classifierV7_fewerclass.py

--- 11_19 Image Classification/classifierV7_fewerclass.py
+++ 11_19 Image Classification/classifierV7_fewerclass.py
@@ -30,145 +30,46 @@ from scipy.stats import skew
 
 # Function to train the classifier
 def train_classifier():
-    images_filepath = '/home/leonardo/Documents/2023Simgs/S2023_imgs/'#'/home/ymhaskar/VisionFollowing/2023Simgs/S2023_imgs/'
-    labels_filepath = '/home/leonardo/Documents/2023Simgs/S2023_imgs/labels.csv'#'/home/ymhaskar/VisionFollowing/2023Simgs/S2023_imgs/Newlabels.csv'
+    images_filepath = '/home/leonardo/Documents/2023Fimgs/'#'/home/ymhaskar/VisionFollowing/2023Simgs/S2023_imgs/'
+    #labels_filepath = '/home/leonardo/Documents/2023Fimgs/labels.csv'
+    labels_filepath = '/home/leonardo/Documents/2023Fimgs/labels2.csv'#'/home/ymhaskar/VisionFollowing/2023Simgs/S2023_imgs/Newlabels.csv'
 
+    classifier_filepath = '/home/leonardo/Documents/2023Simgs/classifier.pkl'
     split = 0.4
 
     # Load data
     data, label = load_data(images_filepath, labels_filepath)
     print('\nImported', data.shape[0], 'training instances')
-
-    # Shuffle and split data
-    data, label = shuffle(data, label)
-    data_train, data_test, label_train, label_test = train_test_split(data, label, test_size=split)
-
-    # Crop images
-    data_train_cropped = [crop_function(image) for image in data_train]
-    data_test_cropped = [crop_function(image) for image in data_test]
+    print(label)
+    data_test_cropped = [crop_function(image) for image in data]
 
     # Feature extraction using color histogram
-    color_hist_features_train = [extract_color_histogram(image).reshape(-1) for image in data_train_cropped]
     color_hist_features_test = [extract_color_histogram(image).reshape(-1) for image in data_test_cropped]
-    print(len(color_hist_features_train[0]))
-    # Feature extraction using SIFT
-    #sift_features_train = [extract_sift_features(image) for image in data_train]
-    #sift_features_test = [extract_sift_features(image) for image in data_test]
 
     # Feature extraction using HOG
-    hog_features_train = [extract_hog_features(image).reshape(-1) for image in data_train]
-    hog_features_test = [extract_hog_features(image).reshape(-1) for image in data_test]
-    print(np.mean(hog_features_train[0]))
+    hog_features_test = [extract_hog_features(image).reshape(-1) for image in data]
 
     # Feature extraction using HOG skewness
-    skewness_features_train = [compute_hog_skewness(image) for image in data_train]
-    #skewness_features_train = normalize_Skew(skewness_features_train[0])
-    skewness_features_test = [compute_hog_skewness(image) for image in data_test]
-    #skewness_features_test = normalize_Skew(skewness_features_test[0])
-
-    print(skewness_features_train)
-    
-    print('color_hist_features_train shape:', color_hist_features_train[0].shape)
-    print('hog_features_train shape:', hog_features_train[0].shape)
-    print('skewness_features_train shape:', skewness_features_train[0].shape)
+    skewness_features_test = [compute_hog_skewness(image) for image in data]
 
     print('color_hist_features_test shape:', color_hist_features_test[0].shape)
     print('hog_features_test shape:', hog_features_test[0].shape)
     print('skewness_features_test shape:', skewness_features_test[0].shape)
 
-    # Concatenate features
-    #features_train = color_hist_features_train
-    #features_test = color_hist_features_test
-    features_train = np.hstack((color_hist_features_train, hog_features_train, skewness_features_train))
-    print('past train')
     features_test = np.hstack((color_hist_features_test, hog_features_test, skewness_features_test))
-    #features_train = np.hstack((color_hist_features_train, sift_features_train, hog_features_train))
-    #features_test = np.hstack((color_hist_features_test, sift_features_test, hog_features_test))
 
-    #print(features_train)
     # Convert label strings to numerical values
     label_encoder = LabelEncoder()
-    label_train_encoded = label_encoder.fit_transform(label_train)
-    label_test_encoded = label_encoder.transform(label_test)
+    label_test_encoded = label_encoder.transform(label)
 
     # Initialize the classifier (RandomForestClassifier in this case)
-    classifier = RandomForestClassifier(n_estimators=100, random_state=42)
-
-    # Train the classifier
-    classifier.fit(features_train, label_train_encoded)
+    classifier = joblib.load(classifier_filepath)
 
     # Evaluate on the test set
     predicted = classifier.predict(features_test)
     print(metrics.classification_report(label_test_encoded, predicted))
     print(metrics.confusion_matrix(label_test_encoded, predicted))
 
-    # Save the trained model
-    joblib.dump(classifier, '/home/leonardo/Documents/2023Simgs/classifier.pkl')
-    print('Saved model classifier.pkl to the current directory.')
-
-def old_crop_function(image):
-    low_H = 0
-    low_S = 99
-    low_V = 119
-    high_H = 10
-    high_S = 255
-    high_V = 236
-    min_size = 0.1
-    no_contour = 0
-    blur = cv2.GaussianBlur(image,(15,15),0)
-    blur_HSV = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-    frame_threshold = cv2.inRange(blur_HSV,(low_H, low_S, low_V),(high_H, high_S, high_V))
-    frame_threshold = apply_closing(frame_threshold, 35)
-    contours = cv2.findContours(frame_threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    if len(contours[0])>0:
-        filtered_contours = [contour for contour in contours[0] if cv2.contourArea(contour) > min_size]
-        if len(filtered_contours)==0:
-            no_contour = 1
-    else:
-        no_contour = 1
-    if no_contour==1:
-        low_H = 20
-        low_S = 90
-        low_V = 0
-        high_H = 126
-        high_S = 255
-        high_V = 255
-
-        frame_threshold = cv2.inRange(blur_HSV,(low_H, low_S, low_V),(high_H, high_S, high_V))
-        frame_threshold = apply_closing(frame_threshold, 35)
-        contours = cv2.findContours(frame_threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        if len(contours[0])>0:
-            filtered_contours = [contour for contour in contours[0] if cv2.contourArea(contour) > min_size]
-            if len(filtered_contours)==0:
-                no_contour = 2
-        else:
-            no_contour = 2
-
-    if no_contour != 2:
-        count = filtered_contours[0]
-        (x_axis,y_axis),radius = cv2.minEnclosingCircle(count)
-        center = (int(x_axis),int(y_axis))
-        radius = int(radius)
-		# reduces likelihood of showing contour on wrong object
-        if radius>10:
-                #cv2.circle(image,center,radius,(0,255,0),2)
-                #cv2.circle(frame_threshold,center,radius,(0,255,0),2)
-            x1,y1,x2,y2 = int(center[0]-radius*1.2), int(center[1]-radius*1.2), int(center[0]+radius*1.2), int(center[1]+radius*1.2)
-            #cropped = image[int(y1):int(y2), int(x1):int(x2)]
-            cropped = blur_HSV[int(y1):int(y2), int(x1):int(x2)]
-
-        else:
-            print("No Contour found")
-            cropped = blur_HSV#image
-    else:
-        print("No Contour found")
-        cropped = blur_HSV#image
-    #cv2.imshow('CHAIN_APPROX_SIMPLE Point only', cropped)
-    #print(no_contour)
-    #cv2.waitKey(0)
-
-    #cropped_HSV = cv2.cvtColor(cropped, cv2.COLOR_BGR2HSV)
-    return cropped
 def crop_function(image):
     low_H = 0
     low_S = 99
@@ -321,10 +222,13 @@ def normalize_Skew(skew_vals):
 # Function to load data from CSV files
 def load_data(images_filepath, labels_filepath):
     labels_data = np.loadtxt(labels_filepath, delimiter=',', dtype=str)
-    images_paths = [images_filepath + f'{number}.png' for number in labels_data[:, 0]]
+    images_paths = [images_filepath + f'{number}.jpg' for number in labels_data[:, 0]]
     images = [cv2.imread(image_path) for image_path in images_paths]
     labels = labels_data[:, 1]
-    return np.array(images), labels
+    labels_array = np.array(labels, dtype=int).reshape(-1,1)
+    print('labels shape:', labels_array.shape)
+    print(labels_array)
+    return np.array(images), labels_array
 
 # Main entry point
 if __name__ == '__main__':
