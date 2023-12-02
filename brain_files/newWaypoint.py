56c0967dd49d2fa718bd17fc1f86fbf95b33409a
jsommer33 <146038709+jsommer33@users.noreply.github.com>
2023-12-01 21:44:37 -0500

    Update newWaypoint.py

--- brain_files/newWaypoint.py
+++ brain_files/newWaypoint.py
@@ -1,21 +1,19 @@
 import math
 import numpy as np
-import matplotlib.pyplot as plt
 
-# Function that takes current point and uses desired direction to output a new position and orientation
-def calculateNewWaypoint(x, y, w, dir):
+# function that takes current point and uses desired direction to output a new position and orientation
+def newWaypt(x, y, w, dir):
+    waypoints = np.array([[1, 1, 0], [1, 2, 0], [2, 1, 0], [2, 2, 0]])
+
     if dir == 1:
-        dw = -math.pi / 2.0
+        dw = -pi / 2.0
         local_p1 = np.array([0, 1, 0, 1])
     elif dir == 2:
-        dw = math.pi / 2.0
+        dw = pi / 2.0
         local_p1 = np.array([0, -1, 0, 1])
     elif dir == 3:
-        dw = math.pi
+        dw = pi
         local_p1 = np.array([-1, 0, 0, 1])
-    else:
-        dw = 0
-        local_p1 = np.array([0, 0, 0, 1])
 
     # Calculate Pnew once outside the loop
     T = np.array([
@@ -25,46 +23,39 @@ def calculateNewWaypoint(x, y, w, dir):
         [0, 0, 0, 1]
     ])
     Pnew = np.dot(T, local_p1)
+    min_error = float('inf')
+    newWaypt = None
+
+    for waypoint in waypoints:
+        x_target, y_target,w_temp = waypoint
+
+        # Calculate error for each waypoint
+        error = math.sqrt((x_target - Pnew[0]) ** 2 + (y_target - Pnew[1]) ** 2)
+        if error < min_error:
+            min_error = error
+            newWaypoint = waypoint
+
+    newWaypoint[2] = w+dw
+
     Wnew = w+dw
     Wnew = np.arctan2(np.sin(Wnew), np.cos(Wnew))
     Pnew[2] = Wnew
-    x_new = Pnew[0]
-    y_new = Pnew[1]
-    w_new = Pnew[2]
-    return x_new, y_new, w_new, dir
+    
+    return Pnew
 
-# Number of iterations
-num_iterations = 5
 
-# Initial position and orientation
+# givens
+pi = math.pi
 x_cur = 1.0
 y_cur = 1.0
 w_cur = 0.0
+dir = 2  # 1 right, 2 left, 3 back, 0 stay
 
-# Expand the plot window
-plt.figure(figsize=(10, 10))
-
-# Plotting
-plt.scatter(x_cur, y_cur, marker='o', color='blue', label='Initial Position')
-scale_factor = 0.02*plt.gcf().get_size_inches()[0]
-for i in range(1, num_iterations+1):
-    dir = np.random.choice([1, 2])
-    x_cur, y_cur, w_cur, dur = calculateNewWaypoint(x_cur, y_cur, w_cur, dir)
-    print(x_cur, y_cur, w_cur,dir)
-    plt.scatter(x_cur, y_cur, marker='o', color='red')
-    #plt.quiver(x_cur, y_cur, np.cos(w_cur), np.sin(w_cur), angles='xy', scale_units='xy', scale=scale_factor, color='green')
-    plt.text(x_cur, y_cur, str(i), fontsize=10, ha = 'right', va = 'bottom')
-
-# Final position and orientation
-plt.scatter(x_cur, y_cur, marker='o', color='orange', label='Final Position')
-#plt.quiver(x_cur, y_cur, np.cos(w_cur), np.sin(w_cur), angles='xy', scale_units='xy', scale=scale_factor, color='green', label='Final Direction')
-plt.text(x_cur, y_cur, 'End', fontsize=10, ha = 'right', va = 'bottom')
-
-plt.axis('equal')
-plt.axis('auto')
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.title('Robot Movement')
-plt.legend()
-plt.grid(True)
-plt.show()
+print(x_cur, y_cur, w_cur)
+# Example usage
+Pnew = newWaypt(x_cur, y_cur, w_cur, dir)
+print(f'New waypoint: {Pnew}')
+x_new = Pnew[0]
+y_new = Pnew[1]
+w_new = Pnew[2]
+print(f'New waypoint: {newWaypt(x_new, y_new, w_new, 3)}')
